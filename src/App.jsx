3e14e2a2cd97cbfe02564eb6513d30e5f1872d7f
hammad38 <hammadsami119@gmail.com>
2022-09-19 17:09:16 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,68 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const App = () => {
   return (
-    <div className='container'>
-        <div className="wrapper">
-            <div className="title">
-            <h1 className="heading">
-                Welcome
-            </h1>
-            <p className="para">
-                This is form validation in react
-            </p>
+    <div className="container">
+      <div className="wrapper">
+        <h1 className="heading">Welcome</h1>
+        <p className="para">This is form validation in react</p>
+
+        <div className="subContainer">
+          <form action="">
+            <div className="input-block">
+              <label htmlFor="name" className="input-label">
+                Name
+              </label>
+              <input
+                className="input"
+                type="name"
+                autoComplete="off"
+                name="name"
+                id="name"
+              />
             </div>
-            <form action="">
-                <div className='input-block'>
-                    <label 
-                    htmlFor="name" className='input-label'>
-                    Name
-                    </label>
-                    <input
-                     type="name" 
-                     autoComplete='off' 
-                     name='name' 
-                     id='name' />
-                </div>
-                <div className='input-block'>
-                    <label 
-                    htmlFor="email" className='input-label'>
-                        Email
-                    </label>
-                    <input 
-                    type="email" 
-                    autoComplete='off'
-                    name="email" 
-                    id="email" />
-                </div>
-                <div className="input-block">
-                    <label 
-                    htmlFor="password"
-                    className='input-label'
-                    >Password
-                    </label>
-                    <input 
-                    type="password"
-                    autoComplete='off'
-                    name='password'
-                    id='password'
-                     />
-
-                </div>
-                <div className="input-block">
-                    <label 
-                    htmlFor="confirmPassword"
-                    className='input-label'
-                    >Confirm Password
-                    </label>
-                    <input 
-                    type="password"
-                    autoComplete='off'
-                    name='confirmPassword'
-                    id='confirmPassword'
-                     />
-
-                </div>
-            </form>
+            <div className="input-block">
+              <label htmlFor="email" className="input-label">
+                Email
+              </label>
+              <input
+                className="input"
+                type="email"
+                autoComplete="off"
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="password" className="input-label">
+                Password
+              </label>
+              <input
+                className="input"
+                type="password"
+                autoComplete="off"
+                name="password"
+                id="password"
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="confirmPassword" className="input-label">
+                Confirm Password
+              </label>
+              <input
+                className="input"
+                type="password"
+                autoComplete="off"
+                name="confirmPassword"
+                id="confirmPassword"
+              />
+            </div>
+          </form>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
